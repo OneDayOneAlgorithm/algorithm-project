8f1c0d709c33b45ae5e0160a2be004f1d62a12cf
Kim hyeongjin <127001416+OneDayOneAlgorithm@users.noreply.github.com>
2023-03-30 00:01:31 +0900

    0330

--- 0328/SW_최대상금.py
+++ 0328/SW_최대상금.py
@@ -1,23 +1,26 @@
-def solve(n):
-    global sum_v
-    if n == change:
-        tmp = int(''.join(arr))
-        if arr < tmp:
-            arr = tmp
+def dfs(n):
+    global ans
+    if n==N:
+        ans = max(ans,int(''.join(map(str,lst))))
         return
-    for i in range(N):
-        for j in range(i+1, N):
-            arr[i], arr[j] = arr[j], arr[i]
-            changed_arr = ''.join(arr)
-            if visited[]
-
-
+    # L개에서 2개뽑는 모든 조합(둘을 교환)
+    for i in range(L-1):
+        for j in range(i+1,L):
+            lst[i], lst[j] = lst[j], lst[i]
+            chk = int(''.join(map(str,lst)))*10+n       # 가지치기
+            if chk not in v:
+                dfs(n+1)
+                v.append(chk)                           # 가지치기
+            lst[i],lst[j] = lst[j], lst[i]  # 원상복구
 T = int(input())
-for tc in range(1, T+1):
-    arr, change = input().split()
-    arr = list(arr)
-    change = int(change)
-    N = len(arr)
-    sum_v = 0
-    visited = [0]*N
-    solve(change)
+for tc in range(1,T+1):
+    st,t=input().split()
+    N = int(t)
+    lst=[]
+    for ch in st:
+        lst.append(int(ch))
+    L = len(lst)
+    ans = 0
+    v = []
+    dfs(0)
+    print(f'#{tc} ')
--- 0329/SW_5204.py
+++ 0329/SW_5204.py
@@ -1,106 +1,52 @@
-def partition(arr):
-    if len(arr) <= 1:
-        return arr
-    mid = len(arr)//2
-    left = partition(arr[:mid])
-    right = partition(arr[mid:])
-    return merge(left,right)
+def partition(lst): 
 
-def merge(left,right):
-    global cnt
-    left_N = len(left)
-    right_N = len(right)
-    result = [0] * (left_N + right_N)
-    left_i, right_i = 0, 0
-    i = 0
-    if left[-1] > right[-1]:
-        cnt += 1
-
-    while left_i<len(left) and right_i<len(right):
-        if left_i < left_N and right_i < right_N:
-            if left[left_i] <= right[right_i]:
-                result[i] = left[left_i]
-                i += 1
-                left_i += 1
-            else:
-                result[i] = right[right_i]
-                i += 1
-                right_i += 1
-
-        elif left_i < left_N:
-            result[i] = left[left_i]
-            i += 1
-            left_i += 1
-        elif right_i < right_N:
-            result[i] = right[right_i]
-            i += 1
-            right_i += 1
-            # print(result)
-        return result
-
-
-
-
-T = int(input())
-for tc in range(1,T+1):
-    N = int(input())
-    arr = list(map(int,input().split()))
-    cnt = 0
-    ans = partition(arr)
-    print(f'#{tc} {ans[N//2]} {cnt}')
-
-
-```
-def partition(lst):
-
-    if len(lst) <= 1:
-        return lst
-    mid = len(lst)//2
-    left_lst = partition(lst[:mid])
+    if len(lst) <= 1:   # 배열의 길이가 1이되면
+        return lst      # 쪼개기를 끝낸다.
+    mid = len(lst)//2   # 배열의 길이가 1이 아닐 시 더 쪼개기 위해 mid 를 구한다.
+    left_lst = partition(lst[:mid]) # 그리고 mid를 기준으로 쪼갠다.
     right_lst = partition(lst[mid:])
-    return merge(left_lst, right_lst)
+    return merge(left_lst, right_lst)   # 쪼갠 두 배열을 정렬한다. 처음엔 1개짜리배열... 그다음 2개짜리배열...
 
 
-def merge(left_lst, right_lst):
-    global cnt
-    left_N = len(left_lst)
-    right_N = len(right_lst)
-    result = [0]*(left_N+right_N)
-    left_i, right_i = 0, 0
+def merge(left_lst, right_lst): # 쪼갠 두 배열을 정렬하는 함수
+    global cnt  
+    left_N = len(left_lst)      # 쪼개고난 왼쪽 배열의 길이
+    right_N = len(right_lst)    # 쪼개고난 오른쪽 배열의 길이
+    result = [0]*(left_N+right_N)   # 정렬할 전체 배열의 길이만큼 0으로 채운다.
+    left_i, right_i = 0, 0          # 인덱스를 0으로 초기화한다.
     i = 0
-    if left_lst[-1] > right_lst[-1]:
+    if left_lst[-1] > right_lst[-1]:    # 문제에서 요구하는 가장 오른쪽 값을 비교해여 왼쪽 배열의 크기가 클때를 센다.
         cnt += 1
 
-    while left_i < left_N or right_i < right_N:
-        if left_i < left_N and right_i < right_N:
-            if left_lst[left_i] <= right_lst[right_i]:
-                result[i] = left_lst[left_i]
-                i += 1
-                left_i += 1
-            else:
+    while left_i < left_N or right_i < right_N:         # 이제 왼쪽배열과 오른쪽 배열을 정렬한다.
+        if left_i < left_N and right_i < right_N:         # 왼쪽 배열과 오른쪽 배열에 원소가 둘 다 남아있으면
+            if left_lst[left_i] <= right_lst[right_i]:      # 왼쪽 배열이 작을 시
+                result[i] = left_lst[left_i]                # 왼쪽배열을 결과값에 넣고
+                i += 1                                      # 전체 인덱스와
+                left_i += 1                                 # 왼쪽 배열 인덱스를 증가시킨다.
+            else:                                          # 오른쪽 배열이 작을 시 (이하 생략)
                 result[i] = right_lst[right_i]
                 i += 1
                 right_i += 1
 
-        elif left_i < left_N:
-            result[i] = left_lst[left_i]
+        elif left_i < left_N:                 # 왼쪽 배열에 원소가 남아있으면
+            result[i] = left_lst[left_i]      # 결과값에 왼쪽 배열 원소들를 모두 추가한다.
             i += 1
             left_i += 1
-        elif right_i < right_N:
+        elif right_i < right_N:                 # 오른쪽 배열에 원소가 남아있으면 (이하생략)
             result[i] = right_lst[right_i]
             i += 1
             right_i += 1
     #print(result)
-    return result
+    return result                           # 결과값을 출력한다.
 
 
 T = int(input())
 
-for test_case in range(1, T + 1):
+for tc in range(1, T + 1):
     cnt = 0
     N = int(input())
     lst = list(map(int, input().split()))
-    Data = partition(lst)
+    Data = partition(lst)   # 재병합 결과 배열을 Data에 저장한다.
 
-    print(f'#{test_case} {Data[N//2]} {cnt}')
-```
+    print(f'#{tc} {Data[N//2]} {cnt}')  # Data의 중간의 값과 cnt를 출력한다.
