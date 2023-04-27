85b1818f86635e68adbed376f3ff2f67ae5cb92f
KwonMinWoo <kwmw0427@naver.com>
2023-04-27 16:42:34 +0900

    1

--- 0418/BOJ_18111.py
+++ 0418/BOJ_18111.py
@@ -1,37 +1,29 @@
 import sys
 input = sys.stdin.readline
-def solve(s,e,B):
-    c = s
-    while c<=e:
-        block = 0
-        block += B
+def solve(B):
+    c = 0
+    min_vv = 9999999
+    while c<=256:
+        block = B
         cnt = 0
-        mid = c
         for i in range(N):
             for j in range(M):
-                if mid > ground[i][j]:
-                    cnt += (mid - ground[i][j])
-                    block -= (mid - ground[i][j])
-                elif mid < ground[i][j]:
-                    cnt += (ground[i][j] - mid)*2
-                    block += (ground[i][j] - mid)
+                if c > ground[i][j]:
+                    cnt += (c - ground[i][j])
+                    block -= (c - ground[i][j])
+                else:
+                    cnt += (ground[i][j] - c)*2
+                    block += (ground[i][j] - c)
         if block >= 0:
-            lst.append((cnt,mid))
+            min_vv = cnt
+            lst.append((cnt,c))
         c += 1
 
 N,M,B = map(int,input().split())
 ground = [list(map(int,input().split())) for _ in range(N)]
-e = 0
-s = 999999
-for i in range(N):
-    for j in range(M):
-        if e < ground[i][j]:
-            e = ground[i][j]
-        if s > ground[i][j]:
-            s = ground[i][j]
 lst = []
-solve(s,e,B)
-lst.sort(key=lambda x:x[1])
+solve(B)
+lst.sort(key=lambda x:x[1], reverse=True)
 lst.sort(key=lambda x:x[0])
 print(*lst[0])
 
